1208fc8255f3571a48f1a41b423627bca85751b5
Jayan <jayanok@gmail.com>
2023-08-26 22:45:41 -0400

    v2 all plots features and several styling added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { useState, useRef } from 'react';
 import Papa from 'papaparse';
 import { useDropzone } from 'react-dropzone';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { Container, AppBar, Toolbar, Typography, Box, Button, Select, MenuItem, Alert, CssBaseline, ThemeProvider, createTheme, Grid } from '@mui/material';
+import { UploadFile } from '@mui/icons-material';
 import Plot from 'react-plotly.js';
+import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
 
 function App() {
   const [csvData, setCsvData] = useState([]);
   const [columns, setColumns] = useState([]);
   const [selectedColumn, setSelectedColumn] = useState(null);
+  const [plotAll, setPlotAll] = useState(false);
   const [uploadedFileName, setUploadedFileName] = useState(null);
-  const [isInputFocused, setInputFocused] = useState(false);
   const chartRef = useRef(null);
 
   const { getRootProps, getInputProps } = useDropzone({
@@ -22,7 +34,6 @@ function App() {
   function onDrop(acceptedFiles) {
     const file = acceptedFiles[0];
     setUploadedFileName(file.name);
-
     Papa.parse(file, {
       complete: (result) => {
         setColumns(result.meta.fields);
@@ -33,90 +44,186 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="navbar navbar-expand-lg navbar-light bg-light custom-navbar">
-        <a className="navbar-brand" href="#">
-        <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" style={{ width: "60px", marginRight: "20px", verticalAlign: "middle" }} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <img src={process.env.PUBLIC_URL + '/logo2.png'} alt="Logo" className="logo" />
+            <Typography variant="h6" className="title">
+              Plots
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box display="flex" flexDirection="column" alignItems="center" mt={3}>
+            <Box className="main-content" sx={{ mt: 2, flexGrow: 4 }}>
+              <Box {...getRootProps()} className="dropzone" sx={{ my: 3 }}>
+              <img src={process.env.PUBLIC_URL + '/myLogo1.png'} alt="Your Logo" className="logo" />
+                <Button variant="contained" color="primary" {...getInputProps()}>
+                  <UploadFile />
+                  Select a CSV File 
+                </Button>
+                <Typography variant="body1"   sx={{
+                  mt: 1,
+                  backgroundColor: '#53c27e', // Set the background color
+                      color: 'white', // Set the text color
+                        fontWeight: 'bold',
+                      padding: '10px 20px', // Add some padding for spacing
+                    borderRadius: '10px', // Round the corners
+                  cursor: 'pointer', // Show a pointer cursor on hover
+  }} className="custom-button">Upload your file</Typography>
+              </Box>
+              {uploadedFileName && (
+                <Alert severity="info" sx={{ mt: 2 }}>
+                  Uploaded File: {uploadedFileName}
+                </Alert>
+              )}
+              {columns.length > 0 && (
+                <Box className="form-group" sx={{ mt: 2 } }>
+                  <Typography variant="body1">Select Column:</Typography>
+                  <Select
+                    value={selectedColumn || 'plot-all'}
+                    onChange={e => {
+                      if (e.target.value === 'plot-all') {
+                        setPlotAll(true);
+                        setSelectedColumn(null);
+                      } else {
+                        setPlotAll(false);
+                        setSelectedColumn(e.target.value);
+                      }
+                    }}
+                    sx={{ mt: 1 }}
+                  >
+                    {columns.map(column => <MenuItem key={column} value={column}>{column}</MenuItem>)}
+                    <MenuItem value="plot-all">Plot All</MenuItem>
+                  </Select>
+                </Box>
+              )}
 
-          Plots
-        </a>
+{selectedColumn && !plotAll && csvData.length > 0 && (
+    <Box className="chart-container" display="flex"
+    flexDirection="column"  alignItems="center" sx={{ mt: 4, height: '500px', mb: 3 }}>
+        <Plot
+            data={[
+                {
+                    x: csvData.map((_, index) => index + 1),
+                    y: csvData.map(row => parseFloat(row[selectedColumn])),
+                    type: 'scatter',
+                    mode: 'lines',
+                    marker: { color: 'rgb(100, 10, 192)' },
+                    line: { color: 'rgba(6, 9, 91, 0.6)' },
+                    name: selectedColumn, 
+                },
+            ]}
+            layout={{
+                autosize: true,
+                margin: { l: 60, r: 10, t: 10, b: 40 },
+                showlegend: true,
+                legend: { x: 1, y: 1 },
+                xaxis: { 
+                    title: 'Time[s]',
+                    tickpadding: 10
+                },
+                yaxis: { 
+                    title: selectedColumn,
+                    tickpadding: 10
+                },
+                responsive: true,
+                shapes: [
+                    {
+                        type: 'rect',
+                        xref: 'paper',
+                        yref: 'paper',
+                        x0: 0,
+                        y0: 0,
+                        x1: 1,
+                        y1: 1,
+                        fillcolor: 'none',
+                        line: {
+                            color: 'lightgreen',
+                            width: 2
+                        }
+                    }
+                ]
+            }}
+        />
+    </Box>
+)}
+
+              {plotAll && csvData.length > 0 && (
+                <Box 
+                display="flex" 
+                flexDirection="column" 
+                justifyContent="center" 
+                alignItems="center"
+                sx={{ mt: 4, height: '100%', mb: 3, width: '100%' }}
+            >
+                 <Container>
+                <Grid container spacing={3} sx={{ mt: 4, padding: 1 }}>
+                  {columns.map(column => (
+                    <Grid item xs={12} md={4} key={column} sx={{ 
+                      border: '1.5px solid lightgreen',  // This adds a 2px solid border
+                      borderRadius: '8px',         // This rounds the corners (optional)
+                      padding: 0.1,                  // Internal padding so the plot doesn't touch the border
+                      margin: 0.1,                   // Increases the margin around the plot container
+                      backgroundColor: '#fff',     // Ensures that the padded area is white (or any color you prefer)
+                     
+                    }} >
+                      <Plot
+                        data={[
+                          {
+                            x: csvData.map((_, index) => index + 1),
+                            y: csvData.map(row => parseFloat(row[column])),
+                            type: 'scatter',
+                            mode: 'lines',
+                            marker: { color: 'rgb(100, 10, 192)' },
+                            line: { color: 'rgba(6, 9, 91, 0.5)' },
+                            name: column,
+                          },
+                        ]}
+                        layout={{
+                          title: column,
+                          autosize: true,
+                          margin: { l: 50, r: 10, t: 50, b: 40 },
+                          xaxis: { title: 'Time[s]' , tickpadding: 1, }, // dtick:1 for  
+
+                          yaxis: { title: column,  tickpadding: 1,  },
+                          plot_bgcolor: '#fcfdff', // Color for the plotting area
+                          margin: {
+                            l: 60,  // Add 10 to your original value
+                            r: 60,  // Add 10 to your original value
+                            t: 60,  // Add 10 to your original value
+                            b: 60   // Add 10 to your original value
+                        },
+                        shapes: [
+                          {
+                              type: 'rect',
+                              xref: 'paper',
+                              yref: 'paper',
+                              x0: 0,
+                              y0: 0,
+                              x1: 1,
+                              y1: 1,
+                              line: {
+                                  color: '#a6b0f5',  // Border color
+                                  width: 1  // Border thickness
+                              }
+                          }
+                      ]
+                          
+                        }}
+                        style={{ width: '100%', height: '300px' }}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+                </Container>
+                </Box>
+              )}
+            </Box>
+          </Box>
       </div>
-
-      <div {...getRootProps()} style={{ border: '2px  gray', padding: '20px', cursor: 'pointer', marginTop: '20px' }}>
-         <label className={`btn btn-primary mt-2 ${isInputFocused ? 'focused' : ''}`} {...getInputProps()}>
-        Select a CSV File 
-        <input id="csvFileInput" style={{ display: 'none' }} onFocus={() => setInputFocused(true)} onBlur={() => setInputFocused(false)} />
-    </label>
-        <input {...getInputProps()} id="csvFileInput" style={{ display: 'none', width: '200px' }} onFocus={() => setInputFocused(true)} onBlur={() => setInputFocused(false)} />
-        <p>Upload your file</p>
-      </div>
-
-      {uploadedFileName && (
-        <div className="alert alert-info">
-          Uploaded File: {uploadedFileName}
-        </div>
-      )}
-
-      {columns.length > 0 && (
-        <div className="form-group">
-          <label>Select Column: </label>
-          <select onChange={e => setSelectedColumn(e.target.value)}>
-            {columns.map(column => <option key={column} value={column}>{column}</option>)}
-          </select>
-        </div>
-      )}
-
-      {selectedColumn && csvData.length > 0 && (
-        <div className="chart-container mt-4 mb-5" style={{ height: '500px', marginBottom: '20px' }}>
-
-<Plot
-    data={[
-        {
-            x: csvData.map((_, index) => index + 1),
-            y: csvData.map(row => parseFloat(row[selectedColumn])),
-            type: 'scatter',
-            mode: 'lines',
-            marker: { color: 'rgb(100, 10, 192)' },
-            line: { color: 'rgba(6, 9, 91, 0.6)' },     //rgba(0, 4, 192, 0.2)
-            name: selectedColumn, 
-        },
-    ]}
-    layout={{
-        autosize: true,
-        margin: { l: 60, r: 10, t: 10, b: 40 },
-        showlegend: true,
-        legend: { x: 1, y: 1 },
-        xaxis: { 
-            title: 'Time[s]',
-            tickpadding: 10  // Adjust as needed
-        },
-        yaxis: { 
-            title: selectedColumn,
-            tickpadding: 10  // Adjust as needed
-        },
-        responsive: true,
-        shapes: [
-            {
-                type: 'rect',
-                xref: 'paper',
-                yref: 'paper',
-                x0: 0,
-                y0: 0,
-                x1: 1,
-                y1: 1,
-                fillcolor: 'none',
-                line: {
-                    color: 'lightgreen',
-                    width: 2
-                }
-            }
-        ]
-    }}
-/>
-
-        </div>
-      )}
-    </div>
+    </ThemeProvider>
   );
 }
 
